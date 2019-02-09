3fad3e54007e8c7d3431f706dde60e19b8305609
bfs07 <bfs.07@hotmail.com>
2019-02-09 20:27:33 -0300

    Added support to change line to fast IO

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import re
 
 def findType(type, text):
+	# find all declarations of type
 	r = re.findall(r''+type+' [^;]*;', text)
 	var = []
 	for i in range(len(r)):
@@ -8,49 +9,57 @@ def findType(type, text):
 		l = r[i].split(',')
 		for x in l:
 			x = re.sub(" ", "", x)
-			ls = re.findall(r'[a-z|A-z|_][a-z|A-z|_|0-9]*', x)
+			ls = re.findall(r'[a-z|A-Z|_][a-z|A-Z|_|0-9]*', x)
 			y = ls[0]
+			# if it was declared as other type it is invalid
 			if y in typeOfVar and typeOfVar[y] != type:
 				invalidVar.append(y) 
 			typeOfVar[y] = type
 			var.append(y)
-
 	return var
 
-typeNames = ['int', 'string', 'char', 'bool', \
-			'short int', 'long int', 'long long', \
-			'long long int', 'double', 'float', 'long double']
-			
-def getCinEntries(text): 
-	r = re.findall(r'cin[\s]*>>[^;]*;', text)
+def getCinEntries(text):
+	# check comma
+	r = re.findall(r'cin[\s]*>>[^;,]*[;,]', text)
+	print ('r = ' , r)
 	return r
-	
+
 def replaceInput(text, cinEntries):
-	for x in cinEntries:
+	for entry in cinEntries:
 		aux = ''
-		x = x[:-1]
+		last = entry[-1]
+		# delete semicolon (;) or comma (,) from line
+		x = entry[:-1]
 		l = x.split('>>')
+		# delete cin from list
+		l.pop(0)
 		for y in l:
-			re.sub(' ', '', y)
-			if y == 'cin'
-				continue
+			# remove all unuseful spaces
+			y = re.sub(' ', '', y)
+			# if it was declared as two different types the program stops
 			if y in invalidVar:
 				assert False
-			aux += y + ' = ' + 'readType'		
-			
-	
-			
+			aux += y + ' = ' + 'readType' + ', '
+		aux = aux[:-2] + last
+		text = re.sub(entry, aux, text)
+	return text
+
+
 table = {}
 typeOfVar = {}
 invalidVar = []
 
+typeNames = ['int', 'string', 'char', 'bool', \
+			'short int', 'long int', 'long long', \
+			'long long int', 'double', 'float', 'long double']
+
 inp = input("Type line\n")
 
-'''
 for type in typeNames:
 	table[type] = findType(type, inp)
-	'''
-getCinEntries(inp)
+
+x = getCinEntries(inp)
+print (replaceInput(inp, x))
 
 print (table)
 print(invalidVar)
