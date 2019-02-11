b5591f47f09c1fe3a60bf433304af71628453a34
Bernardo Flores Salmeron <bfs.07@hotmail.com>
2019-02-10 21:15:39 -0300

    Change input and output to functions working

--- teste.py
+++ teste.py
@@ -1,27 +1,7 @@
 import re
 
-def findType(type, text):
-	# find all declarations of type
-	r = re.findall(r''+type+' [^;]*;', text)
-	var = []
-	for i in range(len(r)):
-		r[i] = re.sub(type + ' ', '', r[i])
-		l = r[i].split(',')
-		for x in l:
-			x = re.sub(" ", "", x)
-			ls = re.findall(r'[a-z|A-Z|_][a-z|A-Z|_|0-9]*', x)
-			y = ls[0]
-			# if it was declared as other type it is invalid
-			if y in typeOfVar and typeOfVar[y] != type:
-				invalidVar.append(y) 
-			typeOfVar[y] = type
-			var.append(y)
-	return var
-
 def getCinEntries(text):
-	# check comma
-	r = re.findall(r'cin[\s]*>>[^;,]*[;,]', text)
-	print ('r = ' , r)
+	r = re.findall(r'cin[\s\t]*>>[^;,]*[;,]', text)
 	return r
 
 def replaceInput(text, cinEntries):
@@ -34,32 +14,63 @@ def replaceInput(text, cinEntries):
 		# delete cin from list
 		l.pop(0)
 		for y in l:
-			# remove all unuseful spaces
-			y = re.sub(' ', '', y)
-			# if it was declared as two different types the program stops
-			if y in invalidVar:
-				assert False
-			aux += y + ' = ' + 'readType' + ', '
+			aux += y + ' = ' + 'readVar(' + y + ')' + ', '
 		aux = aux[:-2] + last
-		text = re.sub(entry, aux, text)
+		text = text.replace(entry, aux)
 	return text
 
+def getCoutEntries(text):
+	r = re.findall(r'cout[\s\t]*<<[^;,]*[;,]', text)
+	return r
+
+def replaceOutput(text, coutEntries):
+	for entry in coutEntries:
+		print (entry)
+		aux = ''
+		last = entry[-1]
+		# delete semicolon (;) or comma (,) from line
+		x = entry[:-1]
+		l = x.split('<<')
+		# delete cout from list
+		l.pop(0)
+		print (l)
+		for y in l:
+			aux += 'writeVar(' + y + ')' + ', '
+		aux = aux[:-2] + last
+		text = text.replace(entry, aux)
+
+	return text
 
 table = {}
 typeOfVar = {}
 invalidVar = []
+usedTypes = []
+
+readFunctionOfType = {
+	'int': 'readSInt()',
+	'string': 'readString()',
+	'char': 'readChar()',
+	'bool': 'readBool()',
+	'short int': 'readSInt()',
+	'long int': 'readSInt()',
+	'long long': 'readSInt()',
+	'long long int': 'readSInt()',
+	'double': 'readFloating()',
+	'float': 'readFloating()',
+	'long double': 'readFloating()',
+}
 
 typeNames = ['int', 'string', 'char', 'bool', \
 			'short int', 'long int', 'long long', \
 			'long long int', 'double', 'float', 'long double']
 
-inp = input("Type line\n")
+inp = (open("input.txt", "r")).read()
 
-for type in typeNames:
-	table[type] = findType(type, inp)
 
 x = getCinEntries(inp)
-print (replaceInput(inp, x))
+inp = (replaceInput(inp, x))
 
-print (table)
-print(invalidVar)
+x = getCoutEntries(inp)
+inp = (replaceOutput(inp, x))
+
+print (inp)
