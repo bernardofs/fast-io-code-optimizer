05b2887f8de2a534ae89bc7b184f44ec01176d9c
bfs.07 <bfs.07@hotmail.com>
2019-02-13 00:01:22 -0300

    Removed unuseful code in teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,17 @@
 import re
 
+def removeSpacesFromVar(name):
+	
+	i = 0
+	while name[i] in ['\t', ' ', '\n']:
+		i += 1
+
+	j = len(name) - 1
+	while name[j] in ['\t', ' ', '\n']:
+		j -= 1
+
+	return name[i : j + 1]	
+
 def getCinEntries(text):
 	r = re.findall(r'cin[\s\t]*>>[^;,]*[;,]', text)
 	return r
@@ -14,7 +26,7 @@ def replaceInput(text, cinEntries):
 		# delete cin from list
 		l.pop(0)
 		for y in l:
-			aux += y + ' = ' + 'readVar(' + y + ')' + ', '
+			aux += 'readVar(' + removeSpacesFromVar(y) + ')' + ', '
 		aux = aux[:-2] + last
 		text = text.replace(entry, aux)
 	return text
@@ -35,37 +47,13 @@ def replaceOutput(text, coutEntries):
 		l.pop(0)
 		print (l)
 		for y in l:
-			aux += 'writeVar(' + y + ')' + ', '
+			aux += 'writeVar(' + removeSpacesFromVar(y) + ')' + ', '
 		aux = aux[:-2] + last
 		text = text.replace(entry, aux)
 
 	return text
 
-table = {}
-typeOfVar = {}
-invalidVar = []
-usedTypes = []
-
-readFunctionOfType = {
-	'int': 'readSInt()',
-	'string': 'readString()',
-	'char': 'readChar()',
-	'bool': 'readBool()',
-	'short int': 'readSInt()',
-	'long int': 'readSInt()',
-	'long long': 'readSInt()',
-	'long long int': 'readSInt()',
-	'double': 'readFloating()',
-	'float': 'readFloating()',
-	'long double': 'readFloating()',
-}
-
-typeNames = ['int', 'string', 'char', 'bool', \
-			'short int', 'long int', 'long long', \
-			'long long int', 'double', 'float', 'long double']
-
 inp = (open("input.txt", "r")).read()
-
 
 x = getCinEntries(inp)
 inp = (replaceInput(inp, x))
